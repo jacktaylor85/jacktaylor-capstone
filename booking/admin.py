c7c9e685f655872797cdee9d8387df446b6cb2d8
Jack Taylor <jackandrewtaylor1@gmail.com>
2024-12-02 16:05:17 +0000

    Booking Models
    Add booking model, add booking service model. change admin file,
    add forms.py to booking

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import BookableService, Booking
 # Register your models here.
+
+admin.site.register(BookableService)
+admin.site.register(Booking)
--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.conf import settings
 # Create your models here.
+class BookableService(models.Model):
+    name = models.CharField(max_length=100)
+    desc = models.TextField(default="Default description")
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    availability = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+class Booking(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    item = models.ForeignKey('BookableService', on_delete=models.CASCADE)
+    booking_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        f'{self.user.username} booked {self.item.name} on {self.booking_date}'
