29336010e47ce93cbf34d0d277c6f8ed80c925b2
Jack Taylor <jackandrewtaylor1@gmail.com>
2024-12-02 17:38:18 +0000

    LogIn and LogOut
    Add functionality for the logout button so that it takes you back to index instead of admin page

--- booking/urls.py
+++ booking/urls.py
@@ -1,9 +1,10 @@
 from . import views
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', 
         views.HomePage.as_view(), name='home'),
         path('login/', views.login_view, name='login'),
         path('register/', views.register_view, name='register'),
+        path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
