59c9e7c14f51729df88dcdce3862823320f4adb5
Jack Taylor <jackandrewtaylor1@gmail.com>
2024-12-05 13:44:49 +0000

    Services static images
    Created functionality of the images from the admin backend. Images now load and static files load in the services page.

--- booking/models.py
+++ booking/models.py
@@ -7,8 +7,9 @@ class BookableService(models.Model):
     description = models.TextField(default="Default description")
     location = models.CharField(max_length=200, default="Location")
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    image = models.ImageField(null=True, blank=True, default='media/elementor-placeholder-image.webp')
+    image = models.ImageField(null=True, blank=True, upload_to='static/images')
     availability = models.BooleanField(default=True)
+    
 
     def __str__(self):
         return self.name
